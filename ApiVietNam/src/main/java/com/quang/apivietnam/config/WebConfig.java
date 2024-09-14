ba4a3737969de1a97436d8e981351b822a63f667
Quang <135381994+quangbm0807@users.noreply.github.com>
2024-09-14 21:36:55 +0700

    CI CD

--- ApiVietNam/src/main/java/com/quang/apivietnam/config/WebConfig.java
+++ ApiVietNam/src/main/java/com/quang/apivietnam/config/WebConfig.java
@@ -11,7 +11,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://api-vietnam-quangbui.up.railway.app")
+                .allowedOrigins("https://api-vietnam-quangbui.up.railway.app")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
