32b3089acea6f1ef94b5d599adec9c237e3652ed
Quang <135381994+quangbm0807@users.noreply.github.com>
2024-09-14 10:10:35 +0700

    Add Repository

---
+++ ApiVietNam/src/main/java/com/quang/apivietnam/repository/AdministrativeRegionRepository.java
@@ -0,0 +1,8 @@
+package com.quang.apivietnam.repository;
+
+import com.quang.apivietnam.model.AdministrativeRegion;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AdministrativeRegionRepository extends JpaRepository<AdministrativeRegion, Integer> {
+
+}
