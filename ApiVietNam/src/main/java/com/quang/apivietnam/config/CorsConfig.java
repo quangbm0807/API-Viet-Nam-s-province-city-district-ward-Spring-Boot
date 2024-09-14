21b69b9ea178d3dc09524b7f9b478df65488eb36
Quang <135381994+quangbm0807@users.noreply.github.com>
2024-09-14 21:19:07 +0700

    Ok

--- ApiVietNam/src/main/java/com/quang/apivietnam/config/CorsConfig.java
+++ ApiVietNam/src/main/java/com/quang/apivietnam/config/CorsConfig.java
@@ -7,18 +7,15 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class CorsConfig {
-
     @Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("*") // Cho phép tất cả các origin, bạn nên thay đổi thành domain cụ thể trong môi trường production
+                        .allowedOrigins("*")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                        .allowedHeaders("*")
-                        .allowCredentials(true)
-                        .maxAge(3600);
+                        .allowedHeaders("*");
             }
         };
     }
